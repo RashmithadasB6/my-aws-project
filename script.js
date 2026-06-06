6cb8bf5acdb168ad196a54f6120668de94e09181
RashmithadasB6 <rashmithadas757@gmail.com>
2026-06-06 19:17:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Website loaded");
+console.log("RashmithadasB6 - Task 12 Loaded");
+document.querySelector('button')?.addEventListener('click', () => {
+    alert('Welcome to my AWS Task 12 project!');
+});
